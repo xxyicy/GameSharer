4c155bdf8cc845a76e2e7ed6195d13e4da9f92a0
xxyicy <xiaox@rose-hulman.edu>
2016-11-06 01:23:49 -0500

    refactor css

---
+++ GameSharer/public/javascripts/items.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    router = express.Router(),
+    mongoose = require('mongoose'), // mongodb connection
+    bodyParser = require('body-parser'), // parse info from POST
+    methodOverride = require('method-override');  // used to manipulate POST data
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body == 'object' && '_method' in req.body) {
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
+
+
+
+module.exports = router;
